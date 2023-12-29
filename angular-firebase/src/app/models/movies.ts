a85abad7057ca5918f3013054a0a11b4c51d9474
gitbernix <gittaberni@gmail.com>
2023-12-29 16:21:53 +0100

    movies

--- angular-firebase/src/app/models/movies.ts
+++ angular-firebase/src/app/models/movies.ts
@@ -19,4 +19,36 @@ export const data: MovieModel[] = [
         description: 'I do not know anything about this movie',
         country: 'USA',
     },
+    {
+        title: 'Pulp Fiction',
+        director: 'Quentin Tarantino',
+        duration: 154,
+        genres: 'Crime, Drama',
+        releaseDate: 1994,
+        description:
+            'The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.',
+        country: 'USA',
+    },
+
+    {
+        title: 'The Dark Knight',
+        director: 'Christopher Nolan',
+        duration: 152,
+        genres: 'Action, Crime, Drama',
+        releaseDate: 2008,
+        description:
+            'When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.',
+        country: 'USA',
+    },
+
+    {
+        title: 'Parasite',
+        director: 'Bong Joon Ho',
+        duration: 132,
+        genres: 'Comedy, Drama, Thriller',
+        releaseDate: 2019,
+        description:
+            'Greed and class discrimination threaten the newly formed symbiotic relationship between the wealthy Park family and the destitute Kim clan.',
+        country: 'South Korea',
+    },
 ];
