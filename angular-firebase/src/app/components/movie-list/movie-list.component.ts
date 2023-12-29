a55332d4ec004e67450138eae9a0df1323a4eed1
gitbernix <gittaberni@gmail.com>
2023-12-29 14:08:59 +0100

    delete

--- angular-firebase/src/app/components/movie-list/movie-list.component.ts
+++ angular-firebase/src/app/components/movie-list/movie-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { deleteDoc, doc } from '@angular/fire/firestore';
+import { Observable, from } from 'rxjs';
 import { MovieModel } from 'src/app/models/movie.model';
 import { MovieService } from 'src/app/services/movie.service';
 
@@ -12,6 +13,17 @@ export class MovieListComponent implements OnInit {
     movies$: Observable<MovieModel[]> = new Observable();
 
     constructor(private movieService: MovieService) {}
+
+    deleteMovie(movieId?: string): void {
+        if (!movieId) {
+            console.error('Movie ID is undefined');
+            return;
+        }
+        this.movieService.deleteMovie(movieId).subscribe(() => {
+            console.log('Movie deleted');
+            this.movies$ = this.movieService.getMovies();
+        });
+    }
 
     ngOnInit(): void {
         this.movies$ = this.movieService.getMovies();
