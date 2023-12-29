d310cbc1df77419e7b49605a88535e2c3fa044b7
gitbernix <gittaberni@gmail.com>
2023-12-29 12:03:52 +0100

    movie-list

--- angular-firebase/src/app/components/movie-list/movie-list.component.ts
+++ angular-firebase/src/app/components/movie-list/movie-list.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { MovieModel } from 'src/app/models/movie.model';
+import { MovieService } from 'src/app/services/movie.service';
 
 @Component({
-  selector: 'app-movie-list',
-  templateUrl: './movie-list.component.html',
-  styleUrls: ['./movie-list.component.css']
+    selector: 'app-movie-list',
+    templateUrl: './movie-list.component.html',
+    styleUrls: ['./movie-list.component.css'],
 })
-export class MovieListComponent {
+export class MovieListComponent implements OnInit {
+    movies$: Observable<MovieModel[]> = new Observable();
 
+    constructor(private movieService: MovieService) {}
+
+    ngOnInit(): void {
+        this.movies$ = this.movieService.getMovies();
+    }
 }
---
+++ angular-firebase/src/app/services/movie.service.ts
@@ -0,0 +1,78 @@
+import { MovieModel } from './../models/movie.model';
+import { Injectable } from '@angular/core';
+
+import { data } from '../models/movies';
+import {
+    addDoc,
+    collection,
+    doc,
+    DocumentData,
+    Firestore,
+    getDocs,
+    writeBatch,
+} from '@angular/fire/firestore';
+
+import { from, map, Observable } from 'rxjs';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class MovieService {
+    private readonly movieCollectionRef = collection(this.firestore, 'movies');
+
+    constructor(private firestore: Firestore) {}
+
+    // //*Drop, intitialize, and upload firebase db
+    public async initializeDb(): Promise<void> {
+        await this.dropCollection('movies');
+        await this.uploadCollection('movies', data);
+    }
+
+    private async dropCollection(collectionName: string): Promise<void> {
+        console.log(`Dropping collection ${collectionName}`);
+
+        const c = collection(this.firestore, collectionName);
+        const snapshot = await getDocs(c);
+
+        const batch = writeBatch(this.firestore);
+        for (let doc of snapshot.docs) {
+            batch.delete(doc.ref);
+        }
+        await batch.commit();
+
+        console.log(`Done!`);
+    }
+
+    private async uploadCollection(collectionName: string, data: MovieModel[]) {
+        console.log(`Uploading collection ${collectionName}`);
+
+        const collectionRef = collection(this.firestore, collectionName);
+        const batch = writeBatch(this.firestore);
+        data.forEach((data) => {
+            const ref = doc(collectionRef);
+            batch.set(ref, data);
+        });
+        await batch.commit();
+
+        console.log(`Done!`);
+    }
+
+    // SAVE
+    addMovie(movie: MovieModel): Observable<DocumentData> {
+        return from(addDoc(this.movieCollectionRef, movie));
+    }
+
+    // GETMovies
+    getMovies(): Observable<MovieModel[]> {
+        return from(getDocs(this.movieCollectionRef)).pipe(
+            map((snapshot) => {
+                const resultList = snapshot.docs.map((doc) => {
+                    const movieData: MovieModel = doc.data() as MovieModel;
+                    movieData.id = doc.id;
+                    return movieData;
+                });
+                return resultList;
+            })
+        );
+    }
+}
