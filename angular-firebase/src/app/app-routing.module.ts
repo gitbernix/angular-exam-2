204ef42e0a0f955e49661a91ee99589794b0c9d4
gitbernix <gittaberni@gmail.com>
2023-12-29 10:37:45 +0100

    menus

--- angular-firebase/src/app/app-routing.module.ts
+++ angular-firebase/src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { CarFormComponent } from './components/car-form/car-form.component';
+import { CarListComponent } from './components/car-list/car-list.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'car-form', component: CarFormComponent },
+    { path: 'car-list', component: CarListComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
 export class AppRoutingModule {}
